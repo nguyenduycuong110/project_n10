d584652cd60a1ba52433fbc77a5dc21f9e20c093
nguyenduycuong110 <nguyenduycuongntb@gmail.com>
2024-11-18 10:36:26 +0700

    Second

--- app/Http/Controllers/Consultation/AuthController.php
+++ app/Http/Controllers/Consultation/AuthController.php
@@ -17,38 +17,5 @@ class AuthController
         return view('consultation.auth.login');
     }
 
-    public function login(Request $request){
-
-        $credentials = [
-            'email' => $request->input('email'),
-            'password' => $request->input('password'),
-            'provider' => 'reception'
-        ];
-        
-
-        if(Auth::guard('reception')->attempt($credentials)){
-
-            $user = Auth::guard('reception')->user();
-
-            $request->session()->regenerate();
-
-            return redirect()->route('reception.index')->with('success', 'Đăng nhập thành công');
-
-        }
-
-        return redirect()->route('reception.auth.login')->with('error','Email hoặc Mật khẩu không chính xác');
-    }
-
-    public function logout(Request $request){
-
-        Auth::guard('reception')->logout();
-
-        $request->session()->forget("session_reception");
-
-        $request->session()->regenerateToken();
-
-        return redirect()->route('reception.auth.login');
-
-    }
-
+    
 }
